1035892f6fa17b95f53b5380bb634b64171ec771
Troy Millergit config --global user.name Troy <millertroy001@yahoo.comgit config -- global user.name Troy>
2023-04-18 18:22:08 -0400

    submission, having issue with final fight function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,53 @@
 "use strict";
 
-// TODO - write your code here.
+const randomDamage = () => {
+  return Math.floor(Math.random() * 10 + 1);
+};
+
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.floor(Math.random() * 2);
+  return randNum === 0 ? opt1 : opt2;
+  //if (randNum === 0) {
+  //  return opt1;
+  //} else {
+  //  return opt2;
+  //}
+};
+
+const attackPlayer = function (health) {
+  return health - randNum;
+};
+
+const logHealth = (player, health) => {
+  return console.log(`${player}health: ${health}`);
+};
+
+const logDeath = (winner, loser) => {
+  return console.log(`${winner} defeated ${loser}`);
+};
+
+const isDead = (health) => {
+  return health <= 0 ? true : false;
+};
+
+function fight(player1, player2, player1Health, player2Health) {
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      let player2Health = attackPlayer(player2Health);
+
+      logHealth(player2, player2Health);
+      if ((player2Health = isDead)) logDeath(player1, player2);
+      break;
+    } else {
+      let player1Health = attackPlayer(player1Health);
+    }
+    logHealth(player1, player2);
+    if ((player1Health = isDead)) {
+      logDeath(player2, player1);
+      break;
+    }
+  }
+}
+
+console.log(fight("Troy", "Brad", 100, 100));
